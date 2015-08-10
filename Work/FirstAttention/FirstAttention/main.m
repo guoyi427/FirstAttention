15ce640bdb2a9f2cbac9568450d0849bed88bb07
郭毅 <guoyi@guoyideMacBook-Pro.local>
2015-08-10 23:29:44 +0800

    第一次上传

---
+++ Work/FirstAttention/FirstAttention/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstAttention
+//
+//  Created by 郭毅 on 15/8/10.
+//  Copyright (c) 2015年 郭毅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
